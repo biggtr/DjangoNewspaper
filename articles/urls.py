903eee8188880f846b25abcf4a5f69190ca97265
bigtorresg <bigtorresg@gmail.com>
2023-08-21 15:44:46 +0200

    crud Added

--- articles/urls.py
+++ articles/urls.py
@@ -1,4 +1,16 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
 
-urlpatterns = [path("", ArticleListView, name="article_list")]
+urlpatterns = [
+    path("", ArticleListView, name="article_list"),
+    path("<int:pk>/", ArticleDetailView, name="article_detail"),
+    path("create/", ArticleCreateView, name="article_create"),
+    path("<int:pk>/update/", ArticleUpdateView, name="article_update"),
+    path("<int:pk>/delete/", ArticleDeleteView, name="article_delete"),
+]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import ArticleForm
 from .models import Article
 
 # Create your views here.
@@ -7,3 +9,45 @@ from .models import Article
 def ArticleListView(request):
     articles = Article.objects.all()
     return render(request, "article_list.html", context={"article_list": articles})
+
+
+def ArticleDetailView(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, "article_detail.html", context={"object": article})
+
+
+@login_required
+def ArticleCreateView(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect("article_list")
+
+    else:
+        form = ArticleForm()
+    return render(request, "article_create.html", context={"form": form})
+
+
+@login_required
+def ArticleUpdateView(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("article_list")
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, "article_update.html", context={"form": form})
+
+
+@login_required
+def ArticleDeleteView(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        article.delete()
+        return redirect("article_list")
+    return render(request, "article_delete.html", context={"article": article})
